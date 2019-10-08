f0cfb84423a0a0110c475cc5237b87095dc07185
thabomonamodi <thabomonamodi0@gmail.com>
2019-10-08 12:21:30 +0200

    server defined listening on port zoba

--- Server.java
+++ Server.java
@@ -8,9 +8,48 @@ public class Server
 	private Socket socket = null;
 	private ServerSocket server = null;
 	private DataInputStream in = null;
-}
 
-    public static void main(String args[])
+	public Server(int port)
+	{
+		try
+		{
+			server = new ServerSocket(port);
+			System.out.println("Server started");
+
+			System.out.println("Waiting for a client to connect...");
+
+			socket = server.accept();
+			System.out.println("Client connection accepted");
+
+			in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+			String line = "";
+
+			while (!line.equals("Done"))
+			{
+				try
+				{
+					line = in.readUTF();
+					System.out.println(line);
+				}
+				catch(IOException i)
+				{
+					System.out.println(i);
+				}
+			}
+
+			socket.close();
+			in.close();
+
+		}
+		catch(IOException i)
+				{
+					System.out.println(i);
+				}
+
+	}
+
+}
+public static void main(String args[])
     {
         Server server = new Server(5000);
     }
